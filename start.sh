82696a1861bb10645d7b91f64604f8620c32b6a5
Antoine Farine <antoine.farine@hotmail.com>
2019-06-11 18:33:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ unzip mc.zip -d /data-temp
 
 mv -v data-temp/*/* data/ && rm -r -f /data-temp && rm mc.zip
 
-jarfile=$(cd /data && ls|grep forge-*.jar)
+jarfile=$(cd /data && ls|grep forge.jar)
 
 cd /data && java -server "-Xmx${INIT_MEM}" "-Xms${MAX_MEM}" -jar $jarfile nogui
